3b542aa48194d549f494ac8d5488b1d8c6e5822b
primawk <pwkuntoro@gmail.com>
2024-11-13 14:58:59 +0700

    create extract markdown function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-from textnode import TextNode, TextType
-from split_nodes_delimiter import split_nodes_delimiter
+
 
 def main():
-    node = TextNode("This is text with a `code block` word", TextType.TEXT)
-    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-    
+    print('hellow')
+   
+      
 
 main()
